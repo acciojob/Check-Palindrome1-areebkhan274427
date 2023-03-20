9a53081b32fc67e4a5ae13b8864fcd7c0dc5194f
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-03-21 00:24:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-
+	let temp="";
+	for(let i=0;i<str.length;i++){
+		if(str.charAt(i)>='a' && str.charAt(i)<='z' || str.charAt(i)>='A' && str.charAt(i)<='Z')
+			temp+=str.charAt(i).toLowerCase();
+	}
+	let arr=temp.split("");
+	let revArr=arr.reverse();
+	let joinArr=arr.join("");
+	if(temp===joinArr){
+		return "true";
+	}
+	return "false";
 }
 module.exports = palindrome
